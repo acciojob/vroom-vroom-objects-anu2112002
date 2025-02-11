0e5cef738550947284aa8fd232c22790cc92e6cc
Anushri <112797477+anu2112002@users.noreply.github.com>
2025-02-11 10:35:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,44 @@
 // Complete the js code
-function Car(make, model) {}
 
-function SportsCar(make, model, topSpeed) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
+
+function SportsCar(make, model, topSpeed) {
+	Car.call(this, make, model);
+	this.topSpeed = topSpeed;
+}
+
+// class Car{
+// 	constructor(make, model)
+// 	{
+// 		this.make = make;
+// 		this.model = model;
+// 	}	
+// }
+
+Car.prototype.getMakeModel = function() 
+{
+	return `${this.make} ${this.model}`;
+}
+
+
+// class SportsCar extends Car{	
+// 	constructor(make, model, topSpeed)
+// 	{
+// 		super(name, model);
+// 		this.topSpeed = topSpeed;
+// 	}
+// }
+
+SportsCar.prototype.getTopSpeed = function() 
+{
+	return this.topSpeed;
+}
+
+// SportsCar.prototype = Object.create(Car.prototype);
+// SportsCar.prototype.constructor = SportsCar;
 
 // Do not change the code below
 window.Car = Car;
